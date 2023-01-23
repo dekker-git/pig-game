0dab94837b9b1d66b906bb3239f5315421a589ef
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-23 11:15:23 -0700

    set up JS to initialize game variables.. 0-0 score and dice hidden

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Selecting elements (score 0, score 1)
+const score0 = document.querySelector("#score-0");
+//getElementById is just a quicker version of querySelector..
+//it doesn't require the #
+const score1 = document.getElementById("score-1");
+const dice = document.querySelector(".dice");
+
+//set initial scores to 0
+score0.textContent = 0;
+score1.textContent = 0;
+
+//set dice hidden initially
+dice.classList.add("hidden");
